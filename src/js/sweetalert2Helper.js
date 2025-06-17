372b9734b5a51c2eaeda06d35dcfbddef87bb9f7
DESKTOP-47A4PN7\1 <jason86100110@gmail.com>
2025-06-17 17:57:11 +0800

    後臺成功連接上，修改今天的問題。

--- src/js/sweetalert2Helper.js
+++ src/js/sweetalert2Helper.js
@@ -25,7 +25,7 @@ const SwalHelper = {
     closeButton.id = "swal-close-modal";
 
     // 使用與 ActPopup 相同的 SVG 圖片和樣式
-    closeButton.innerHTML = `<img src="https://lab-event.udn.com/bd_500bowls_2025/image/close_btn.svg" alt="關閉" style="width: 100%; height: 100%;" />`;
+    closeButton.innerHTML = `<img src="https://lab-event.udn.com/bd_500bowls_vote2025_test/image/close_btn.svg" alt="關閉" style="width: 100%; height: 100%;" />`;
 
     // 應用與 ActPopup 完全相同的內聯樣式
     closeButton.style.cssText = `
@@ -290,15 +290,15 @@ const SwalHelper = {
         html: `<div class="swal-vote-success-wrapper">
           <img src="./image/popup_title.png" alt="投票標題圖示" class="vs-title-image">
           <p class="vs-title">投票成功！</p>
-          <h3 class="vs-prize-title">恭喜獲得 <span class="linepoints">LINE POINTS 5 點</span> 及<br />iPhone 16等大獎抽獎資格</h3>
+          <h3 class="vs-prize-title">恭喜獲得 <span class="linepoints">LINE POINTS 5 點</span> 及<br />iPhone 16 等大獎抽獎資格</h3>
           <ul class="vs-info-list">
             <li class="vs-info-item">* LINE 點數兌換序號將於活動後派發。</li>
-            <li class="vs-info-item">*請於活動截止前，<br class="brblock" />至【會員中心】設定點數領取資料，<br />
+            <li class="vs-info-item">* 請於活動截止前，<br class="brblock" />至【會員中心】設定點數領取資料，<br />
                填妥你的 e-mail 和手機號碼！<br class="brblock" /><a class="vs-info-link" href="https://member.udn.com/member/ShowMember?actiontype=update" target="_blank">前往會員中心 >></a>
             </li>
           </ul>
           <div class="vs-action-buttons">
-            <a href="https://event.udn.com/bd_500bowls_vote2025?utm_source=eventsite&utm_medium=button&utm_campaign=bd_500bowls_vote2025" class="vs-action-btn" target="_blank">回活動 >></a>
+            <a href="https://lab-event.udn.com/bd_500bowls_vote2025_test/" class="vs-action-btn">回活動 >></a>
             <a href="https://500times.udn.com/wtimes/cate/123497?utm_source=udn_bd&utm_medium=button&utm_campaign=bd_500bowls_vote2025" class="vs-action-btn" target="_blank">看更多 >></a>
           </div>
         </div>`,
@@ -337,7 +337,7 @@ const SwalHelper = {
           <h2 class="av-title">你今天已經投票囉</h2>
           <p class="av-subtitle">每天都有一次投票機會！<br />明天再回來支持你喜愛的美食吧！</p>
           <div class="av-action-buttons">
-              <a href="https://event.udn.com/bd_500bowls_vote2025?utm_source=eventsite&utm_medium=button&utm_campaign=bd_500bowls_vote2025" class="av-action-btn" target="_blank">回活動 >></a>
+              <a href="https://lab-event.udn.com/bd_500bowls_vote2025_test/" class="av-action-btn">回活動 >></a>
             <a href="https://500times.udn.com/wtimes/cate/123497?utm_source=udn_bd&utm_medium=button&utm_campaign=bd_500bowls_vote2025" class="av-action-btn" target="_blank">看更多 >></a>
           </div>
         </div>`,
